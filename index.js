34231d48ee52b2690868ee2d602aa47a602ae511
zawadinjagi <zawadinjagi619@gmail.com>
2024-03-19 12:26:23 +0300

    add

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 31
+const num2 = 2
+const num3 = 32
+const num4 = 7
+const multiply =num1 * num2
+// will return multiply= 62
 
-// Write your code here
+const random = Math.ceil(Math.random() * 10) + 1
+const mod = num3 % num4
+const max = Math.max(5, 10,15,17,19, 19.9,20)
